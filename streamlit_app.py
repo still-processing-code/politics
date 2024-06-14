d9885f3b1d9b6da0c90de5c388d1fb89552b2de7
still-processing-code <niklas@still-processing.com>
2024-06-14 10:53:22 +0000

    new prompt 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,14 +16,13 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-     
-     prompt = "Please answer the following question like a politician in two sentences. This means that you go into the topic as little as possible and don't say anything important about it, but just talk around it. The question:" + prompt
-
+    prompt = "Please answer the following question like a politician in two sentences. This means that you go into the topic as little as possible and don't say anything important about it, but just talk around it. The question:" + prompt
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
     
-   
+    
+
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
